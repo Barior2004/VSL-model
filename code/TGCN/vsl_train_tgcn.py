d1c03d7f61c6c8a3341e4cfb10f02a00a8093bc6
Trinh Quoc Bao <quocbao061104@gmail.com>
2026-07-14 18:41:32 +0700

    Sửa lại đường dẫn root trong vsl_train_tgcn.py cho Kaggle

--- code/TGCN/vsl_train_tgcn.py
+++ code/TGCN/vsl_train_tgcn.py
@@ -104,7 +104,7 @@ def run(split_file, pose_data_root, configs, save_model_to=None):
 
 
 if __name__ == "__main__":
-    root = '/media/anudisk/github/WLASL'
+    root = '.'
 
     subset = 'vsl472'
 
